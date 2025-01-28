28e429e355889e3dda81639550e4fe61d46d2220
FirdausIqbal <firdausiqbal10@gmail.com>
2025-01-29 04:28:55 +0700

    First Commit

---
+++ src/Model/Card.java
@@ -0,0 +1,34 @@
+package Model;
+
+public class Card {
+    private String value;
+    private String type;
+
+    public Card(String value, String type) {
+        this.value = value;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return value + "-" + type;
+    }
+
+    public String getImagePath() {
+        return "../Images/" + toString() + ",png";
+    }
+
+    public int getValue() {
+        if ("JQKA".contains(value)) {
+            if (value.equals("A")) {
+                return 11;
+            }
+            return 10;
+        }
+        return Integer.parseInt(value);
+    }
+
+    public boolean isAce() {
+        return value.equals("A");
+    }
+}
